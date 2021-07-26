c1cde16a58184af1ccb26b05d70a6757a7c8602a
Divya <singhaldivya11@gmail.com>
2021-07-26 18:28:52 +0000

    Notes app registering and clearing fields

---
+++ public/js/writeNote.js
@@ -0,0 +1,38 @@
+let googleUser = null;
+
+console.log("writeNotes.js is loaded")
+
+window.onload = () => {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user){
+            //this code runs if the user is logged in 
+            console.log("logged in as ", user.displayName)
+            googleUser = user;
+        }
+        else{
+            //this code runs if the user isn't logged in
+            console.log("not logged in")
+        }
+    })
+
+    const createNoteButton = document.querySelector("#createNoteButton")
+    createNoteButton.addEventListener("click", () => {
+        //get values from the form.
+        const noteTitle = document.querySelector("#noteTitle").value
+        const noteText = document.querySelector("#noteText").value
+        console.log(noteTitle, noteText)
+
+        //Write these values to the databse
+        firebase.database().ref(`/users/${googleUser.uid}`).push({
+            title: noteTitle,
+            text: noteText
+        }).then(() => {
+            console.log("database write successful")
+            document.querySelector("#noteTitle").value = ""
+            document.querySelector("#noteText").value = ""
+        })
+        .catch(error => {
+            console.log("error writing new note: ", error)
+        })
+    })
+}
